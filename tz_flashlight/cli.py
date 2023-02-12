4db3543e70a5ecd12b9eb8a1d251b9f36113a327
Maxim Mentus <nyaerffs@gmail.com>
2023-02-12 20:23:39 +0300

    Исправлен порядок импорта, добавлен readme.md, добавлены тесты

--- tz_flashlight/cli.py
+++ tz_flashlight/cli.py
@@ -1,7 +1,10 @@
-import click
 import asyncio
-from tz_flashlight.client import main
+import click
+import unittest
 import uvicorn
+
+from tz_flashlight.client import main
+from tests.testclient import TestClient
 
 @click.group()
 @click.help_option(
@@ -15,10 +18,7 @@ def cli(**kwargs):
 @click.option("-h", "--host", default="localhost", help="Адрес сервера")
 @click.option("-p", "--port", default="9999", help="Порт сервера")
 def runserver(**kwargs):
-    # Проверка валидности порта
-    # ...
-    # ...
-
+    assert 0 < int(kwargs["port"]) < 65535, "Задан неверный порт"
     uvicorn.run(f'tz_flashlight.server:app', host=kwargs["host"], port=int(kwargs["port"]))
 
 @cli.command()
@@ -26,6 +26,10 @@ def runserver(**kwargs):
 @click.option("-p", "--port", default="9999", help="Порт сервера")
 def runclient(**kwargs):
     asyncio.run(main(host=kwargs["host"], port=kwargs["port"]))
+
+# @cli.command()
+# def runtests(**kwargs):
+#     unittest.main(TestClient())
 
 if __name__ == "__main__":
     cli()
